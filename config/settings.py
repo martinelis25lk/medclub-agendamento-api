6eac4a5abbc1a12129c14a0be87ba9a6337d761b
martinelis25lk <gui.almeida.edu@gmail.com>
2026-08-25 12:48:57 -0300

    feat: add swagger documentation with drf-yasg

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'scheduling',
+    'drf_yasg',
+
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-from django.urls import path, include  # <-- Certifique-se de que 'include' está aqui
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from scheduling.views import (
-    EspecialistaViewSet, 
-    AgendaViewSet, 
-    HorarioAtendimentoViewSet, 
-    AgendamentoViewSet
+from scheduling.views import EspecialistaViewSet, AgendaViewSet, HorarioAtendimentoViewSet, AgendamentoViewSet
+
+# Importações do Swagger
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Medclub Agendamento API",
+      default_version='v1',
+      description="API para o sistema de agendamento de especialistas.",
+      contact=openapi.Contact(email="seu-email@exemplo.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
 )
 
 router = DefaultRouter()
@@ -18,4 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/auth/', include('accounts.urls')),
+    
+    # Rota da Documentação Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
