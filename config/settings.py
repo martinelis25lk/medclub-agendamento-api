b56e5ecee5d2d541a92a8dcc3bdd200e82c2a0e6
martinelis25lk <gui.almeida.edu@gmail.com>
2026-08-25 22:27:32 -0300

    feat: add CORS support for local Vue frontend

--- config/settings.py
+++ config/settings.py
@@ -46,10 +46,12 @@ INSTALLED_APPS = [
     'accounts',
     'scheduling',
     'drf_yasg',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -149,3 +151,8 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
